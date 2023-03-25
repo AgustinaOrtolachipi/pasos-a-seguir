e3c0b0685d9df8aad0e5db217ae1d50a034fa646
AgustinaOrtolachipi <agusortolachipi10@gmail.com>
2023-03-25 15:37:26 -0300

    first commit

---
+++ pasosAseguir.js
@@ -0,0 +1,6 @@
+console.log("pasosAseguir");
+console.log("paso one");
+console.log("paso two");
+console.log("paso three");
+console.log("paso four");
+console.log("paso five");
---
+++ pasosAseguir.ts
@@ -0,0 +1,5 @@
+console.log("pasosAseguir");
+console.log("paso1");
+console.log("paso2");
+console.log("paso3");
+console.log("paso4");
